7b1fc2336e66a779bb918e7a8ee003c27b589683
AsherAnd <ashertew@gmail.com>
2023-06-24 21:35:45 -0400

    added parallax image

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,30 @@
 const header = document.querySelector("header");
+const parallaxElem = document.querySelectorAll(".parallax");
+const comicCover = document.getElementById("comic-cover");
+let mouseX = 0,
+  mouseY = 0;
 
 window.addEventListener("scroll", stickyNavbar);
+
+comicCover.addEventListener("mousemove", (e) => {
+  let rect = comicCover.getBoundingClientRect();
+  x = e.clientX - rect.left;
+  y = e.clientY - rect.top;
+
+  mouseX = x - comicCover.clientWidth / 2;
+  mouseY = y - comicCover.clientHeight / 2;
+
+  parallaxElem.forEach((element) => {
+    let speedX = element.dataset.speedx;
+    let speedY = element.dataset.speedy;
+
+    element.style.transform = `translateX(${mouseX * speedX}px) translateY(${
+      mouseY * speedY
+    }px)`;
+  });
+
+  console.log(mouseX, mouseY);
+});
 
 // keep navbar on top when scrolling
 function stickyNavbar() {
