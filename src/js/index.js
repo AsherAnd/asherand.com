55a51f95edf27030f21354387f69ae74230febec
AsherAnd <ashertew@gmail.com>
2023-06-26 23:02:03 -0400

    added light mode theme

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 const header = document.querySelector("header");
 const links = document.querySelectorAll(".nav-link");
+const toggleBtn = document.querySelector(".toggle-btn");
 let mouseX = 0,
   mouseY = 0;
 
@@ -17,13 +18,29 @@ let mixer = mixitup(".projects-gallery", {
   },
 });
 
+// when the site is scrolled
 window.addEventListener("scroll", () => {
   stickyNavbar();
+});
+
+// when the theme button is clicked
+toggleBtn.addEventListener("click", () => {
+  changeTheme();
 });
 
 // keep navbar on top when scrolling
 function stickyNavbar() {
   header.classList.toggle("scrolled", window.pageYOffset > 0);
+}
+
+function changeTheme() {
+  if (document.body.classList.contains("light")) {
+    document.querySelector("body").classList = "dark";
+    toggleBtn.classList.replace("fa-moon", "fa-sun");
+  } else {
+    document.querySelector("body").classList = "light";
+    toggleBtn.classList.replace("fa-sun", "fa-moon");
+  }
 }
 
 scrollReveal.reveal(".showcase-image", { origin: "top", delay: 250 });
