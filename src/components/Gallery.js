931f22d4134fea4220f991c1bc34559460623e1f
AsherAnd <ashertew@gmail.com>
2023-07-15 02:15:00 -0400

    removed responsive css and added new experience card system

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,24 +1,18 @@
 import { ProjectCard } from "../components/Cards";
 import { ExperienceCard } from "../components/Cards";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import { motion } from "framer-motion";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-const testImage = require("../assets/images/Coming Soon.png");
 
 function ProjectGallery(props) {
   return (
     <motion.div layout className="projects-gallery">
-      {props.projects?.map((project) => {
+      {props.projects?.map((project, i) => {
         return (
           <ProjectCard
             img={project.thumbnail}
             alt="image"
             title={project.title}
             link=""
-            key={project.title}
+            key={i}
           />
         );
       })}
@@ -26,35 +20,24 @@ function ProjectGallery(props) {
   );
 }
 
-function ExperienceGallery() {
+function ExperienceGallery(props) {
   return (
     <div className="experience-gallery">
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        centeredSlides={true}
-        // autoplay={{
-        //   delay: 5000,
-        //   disableOnInteraction: false,
-        // }}
-        navigation={true}
-        loop={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <ExperienceCard title="Test Title" img={testImage} alt="test image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <ExperienceCard title="Test Title" img={testImage} alt="test image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <ExperienceCard title="Test Title" img={testImage} alt="test image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <ExperienceCard title="Test Title" img={testImage} alt="test image" />
-        </SwiperSlide>
-      </Swiper>
+      <ExperienceCard
+        title="Test"
+        img="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
+        alt="image"
+      />
+      {/* {props.experience?.map((experience, i) => {
+        return (
+          <ExperienceCard
+            title={experience.title}
+            img={experience.images[0]}
+            alt="image"
+            key={i}
+          />
+        );
+      })} */}
     </div>
   );
 }
--- src/containers/Experience.js
+++ src/containers/Experience.js
@@ -1,7 +1,22 @@
 import { Heading, SubHeading } from "../components/Headings";
 import { ExperienceGallery } from "../components/Gallery";
+import { useEffect, useState } from "react";
 
 function Experience() {
+  const [experience, setExperience] = useState([]);
+
+  useEffect(() => {
+    fetchExperience();
+  }, []);
+
+  const fetchExperience = async () => {
+    const data = await fetch(
+      "https://personal-site-51a24-default-rtdb.firebaseio.com/experience.json"
+    );
+    const experienceJson = await data.json();
+    setExperience(experienceJson);
+  };
+
   return (
     <section className="experience" id="experience">
       <div className="container">
@@ -9,7 +24,7 @@ function Experience() {
           <SubHeading text="Experience"></SubHeading>
           <Heading first="What I Learned" second="What I Gained"></Heading>
         </div>
-        <ExperienceGallery />
+        <ExperienceGallery experience={experience} />
       </div>
     </section>
   );
