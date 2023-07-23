e3b6fc4124060b012f9e6b7537c76efa56ea8ee7
AsherAnd <ashertew@gmail.com>
2023-07-23 11:01:03 -0400

    added menu toggle when section is selected on mobile devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ThemeButton from "./ThemeButton";
 import Hamburger from "./Hamburger";
 
 function Header(props) {
+  var windowWidth = window.innerWidth;
+  const screenSize = 768;
+
   // Sticky Navbar
   const [fixed, setScroll] = useState(true);
+
+  window.addEventListener("resize", () => {
+    windowWidth = window.innerWidth;
+  });
 
   window.addEventListener("scroll", () => {
     if (window.pageYOffset > 0) {
@@ -14,33 +21,37 @@ function Header(props) {
     }
   });
 
+  const toggleMenu = () => {
+    if (windowWidth <= screenSize) props.toggleMenu();
+  };
+
   return (
     <header className={fixed ? "" : "scrolled"}>
       <nav className="container">
         <div className="name">ASHER</div>
         <ul className="pages">
           <li>
-            <a href="#home" className="nav-link">
+            <a href="#home" className="nav-link" onClick={toggleMenu}>
               Home
             </a>
           </li>
           <li>
-            <a href="#about" className="nav-link">
+            <a href="#about" className="nav-link" onClick={toggleMenu}>
               About
             </a>
           </li>
           <li>
-            <a href="#experience" className="nav-link">
+            <a href="#experience" className="nav-link" onClick={toggleMenu}>
               Experience
             </a>
           </li>
           <li>
-            <a href="#projects" className="nav-link">
+            <a href="#projects" className="nav-link" onClick={toggleMenu}>
               Projects
             </a>
           </li>
           <li>
-            <a href="#contact" className="nav-link">
+            <a href="#contact" className="nav-link" onClick={toggleMenu}>
               Contact
             </a>
           </li>
