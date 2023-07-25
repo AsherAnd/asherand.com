48886dbbd17ef520d9ce433b387b2c471f192fcd
AsherAnd <ashertew@gmail.com>
2023-07-25 10:27:31 -0400

    edited comic images, changed dialog image size and changed gallery autoplay time

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -45,7 +45,7 @@ function ExperienceGallery(props) {
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 5000,
           disableOnInteraction: true,
         }}
         pagination={pagination}
