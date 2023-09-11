a93af71eaf453ae76fd4da131cb8e96ce7b6790b
AsherAnd <ashertew@gmail.com>
2023-09-11 15:26:32 -0400

    reverted images seperate layer images

--- frontend/src/components/ComicCover.jsx
+++ frontend/src/components/ComicCover.jsx
@@ -6,10 +6,41 @@ function ComicCover(props) {
       <div className="comic-cover shine"></div>
       <img
         src={
-          props.theme === "dark" ? CoverImages.FullDark : CoverImages.FullLight
+          props.theme === "dark"
+            ? CoverImages.BackgroundColorDark
+            : CoverImages.BackgroundColorLight
+        }
+        alt="background color"
+        className="comic-cover bg-color"
+      />
+      <img
+        src={CoverImages.Background}
+        alt="background"
+        className="comic-cover bg"
+      />
+      <img
+        src={
+          props.theme === "dark"
+            ? CoverImages.AsherDark
+            : CoverImages.AsherLight
         }
         alt="Portrait of Asher Andargachew"
-        className="comic-cover full"
+        className="comic-cover asher"
+      />
+      <img
+        src={CoverImages.ComicsCode}
+        alt="comicscode"
+        className="comic-cover comics-code"
+      />
+      <img
+        src={CoverImages.Issue}
+        alt="issue number"
+        className="comic-cover issue-no"
+      />
+      <img
+        src={CoverImages.Plastic}
+        alt="plastic"
+        className="comic-cover plastic"
       />
     </>
   );
