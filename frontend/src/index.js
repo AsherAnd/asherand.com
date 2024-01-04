d2864e989f6b97e32b0e8f30058873e1a85f72ca
AsherAnd <ashertew@gmail.com>
2024-01-04 03:13:42 -0500

    preloading fonts for faster website loading

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/style.css";
-import "./assets/fonts/FuriousStyles/FuriousSyles.css";
-import "./assets/fonts/ProductSans/ProductSans.css";
 import App from "./App";
+import(
+  /* webpackPreload: true */ "./assets/fonts/FuriousStyles/FuriousSyles.css"
+);
+import(/* webpackPreload: true */ "./assets/fonts/ProductSans/ProductSans.css");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
