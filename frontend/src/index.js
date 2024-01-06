cab00afeefa0c71c662b7cf27f5f7ad87c4ccdf8
AsherAnd <ashertew@gmail.com>
2024-01-06 02:00:14 -0500

    removed single font css import and added it to bigger css file which loaded faster, added aspect ratio to images, changed sections spacing to 8rem

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/style.css";
 import App from "./App";
-import(
-  /* webpackPreload: true */ "./assets/fonts/FuriousStyles/FuriousSyles.css"
-);
-import(/* webpackPreload: true */ "./assets/fonts/ProductSans/ProductSans.css");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
