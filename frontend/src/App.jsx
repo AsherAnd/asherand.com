df0a0a5789a546eaaa4b6e007338fe12dc04259b
asherand <ashertew@gmail.com>
2024-10-30 11:21:51 -0400

    added footer and link style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import About from "./containers/About.jsx";
 import Experience from "./containers/Experience.jsx";
 import Projects from "./containers/Projects.jsx";
 import Contact from "./containers/Contact.jsx";
+import Footer from "./components/footer.jsx";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <Experience />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer>
+      <h3 className="footer-text">
+        <div>
+          Made with <a href="">Test</a>
+        </div>
+      </h3>
+    </footer>
+  );
+}
