86d8027b28ef6b6f1cdfea430e4b8ad40933bc50
AsherAnd <ashertew@gmail.com>
2025-05-11 13:38:26 -0400

    improved blog post image inclusion and loading

--- frontend/src/components/BlogPost.jsx
+++ frontend/src/components/BlogPost.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useLoaderData, useParams, useNavigate } from "react-router";
 import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import "../styles/components/blogpost.css";
 
@@ -28,7 +29,6 @@ export default function BlogPost() {
   const post = useLoaderData();
   const navigate = useNavigate();
   const [markdown, setMarkdown] = useState("");
-  const [imageCache, setImageCache] = useState({});
 
   const goBack = () => {
     navigate(-1);
@@ -53,41 +53,6 @@ export default function BlogPost() {
     }
   }, [post]);
 
-  // Custom image component for react-markdown
-  const ImageWithAuth = ({ src, alt }) => {
-    const [blobUrl, setBlobUrl] = useState(null);
-
-    useEffect(() => {
-      const fetchImage = async () => {
-        if (!src) return;
-
-        if (imageCache[src]) {
-          setBlobUrl(imageCache[src]);
-          return;
-        }
-
-        try {
-          const response = await fetch(`${APIUrl}${src}`);
-          if (!response.ok) throw new Error(`Image fetch failed: ${src}`);
-          const blob = await response.blob();
-          const objectUrl = URL.createObjectURL(blob);
-          setImageCache((prev) => ({ ...prev, [src]: objectUrl }));
-          setBlobUrl(objectUrl);
-        } catch (err) {
-          console.error(err);
-        }
-      };
-
-      fetchImage();
-    }, [src]);
-
-    return blobUrl ? (
-      <img src={blobUrl} alt={alt} />
-    ) : (
-      <span>Loading image...</span>
-    );
-  };
-
   if (!post) {
     throw Error("Blog post doesn't exist");
   }
@@ -99,16 +64,8 @@ export default function BlogPost() {
         <div className="post-title">
           <h1>{post.title}</h1>
         </div>
-        <div className="post-meta">
-          {post.author && <span className="post-author">By {post.author}</span>}
-          {post.date && (
-            <span className="post-date">
-              <h2>{post.date}</h2>
-            </span>
-          )}
-        </div>
         <div className="post-content">
-          <Markdown rehypePlugins={[rehypeRaw]} components={{}}>
+          <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
             {markdown}
           </Markdown>
         </div>
